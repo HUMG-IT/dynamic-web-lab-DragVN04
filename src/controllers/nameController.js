3c8e9d6ff356a3f86d30cf150256fc04beca2c66
LongLe04 <longvis2004@gmail.com>
2024-11-10 16:08:42 +0700

    2221050170 Lê Việt Long

--- src/controllers/nameController.js
+++ src/controllers/nameController.js
@@ -32,8 +32,8 @@ const nameController = require('./controllers/nameController');
  */
 const submitName = (req, res) => {
     const name = req.body.name; // Lấy tên từ yêu cầu của client
-    names.addName(name);        // Thêm tên vào danh sách trong model names
-    res.json({ message: `Xin chào, ${name}!`, names: names.getNames() }); // Trả về JSON với lời chào và danh sách tên
+    nameController.addName(name);        // Thêm tên vào danh sách trong model names
+    res.json({ message: `Xin chào, ${name}!`, names: nameController.getNames() }); // Trả về JSON với lời chào và danh sách tên
 };
 
 module.exports = { submitName };
